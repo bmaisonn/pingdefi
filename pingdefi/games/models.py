21f6088967bc6b7a2f7c689adef8882fd7a836e5
Bertrand Maisonneuve <bmaisonn@gmail.com>
2024-09-01 21:34:29 +0200

    wip

--- pingdefi/games/models.py
+++ pingdefi/games/models.py
@@ -6,8 +6,8 @@ class User(models.Model):
 
 class UsersGame(models.Model):
     time = models.DateTimeField
-    user1 = models.ForeignKey(User)
-    user2 = models.ForeignKey(User)
+    user1 = models.ForeignKey(User, related_name="user1", on_delete=models.SET_NULL, null=True)
+    user2 = models.ForeignKey(User, related_name="user2", on_delete=models.SET_NULL, null=True)
+    winner = models.ForeignKey(User, related_name="winner", on_delete=models.SET_NULL, null=True)
     score = models.JSONField()
-    winner = models.ForeignKey(User)
 
